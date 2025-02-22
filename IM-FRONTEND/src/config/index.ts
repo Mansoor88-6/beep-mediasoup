d7be79c7afd01fdadac3a4826951ccc31f77dc00
Mansoor <mansoor@cytomate.net>
2025-02-22 17:01:19 +0500

    url hardcoded

--- IM-FRONTEND/src/config/index.ts
+++ IM-FRONTEND/src/config/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // eslint-disable-next-line no-process-env
 export const env = process.env.REACT_APP_CUSTOM_ENVIRONMENT;
-export const backendUrl = process.env.REACT_APP_BACKEND_API as string;
+export const backendUrl = 'https://imvc-wj.averox.com';
 
 export const BackendInstance = axios.create({
   baseURL: `${backendUrl}/api/`,
